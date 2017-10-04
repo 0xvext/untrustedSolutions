b31750cb0f9b958d5f6324d51d5a4b5265adc67d
0xvext <0xvext@users.noreply.github.com>
2017-10-03 22:42:49 -0500

    Create untrusted-10-ambush.js

---
+++ untrusted-10-ambush.js
@@ -0,0 +1,17 @@
+#Redefine the actions of the attackDone in the first code section:
+	if (me.canMove('up')) {
+    	me.move('up');
+    }
+    else if (me.canMove('left')) {
+        me.move('left');
+    }
+
+#Redefine the actions of the reinforcementDrone to do nothing.
+
+#Redefine the actions of the defenseDrone in the third code section:
+ 	if (me.canMove('up')) {
+    	me.move('up');
+    }
+    else if (me.canMove('right')) {
+    	me.move('right');
+    }
